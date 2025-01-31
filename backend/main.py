3af2bdcb99dbcd7cb7f8ca153b44e75b079ce8a0
dr-Sax <nicoromano2000@gmail.com>
2025-01-31 13:33:36 -0500

    backend port changew3

--- backend/main.py
+++ backend/main.py
@@ -8,9 +8,10 @@ CORS(app)
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def serve(path):
-    if path != "" and os.path.exists(f"frontend/dist/{path}"):
-        return send_from_directory('frontend/dist', path)
-    return send_from_directory('frontend/dist', 'index.html')
+    static_dir = '/app/frontend/dist'  # Docker container path
+    if path and os.path.exists(os.path.join(static_dir, path)):
+        return send_from_directory(static_dir, path)
+    return send_from_directory(static_dir, 'index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=os.getenv('PORT', 5000))

da0b803cd9bb0237d737a0f4a0d8037e213ba09d
meganrock <rockmegan45@gmail.com>
2025-02-01 18:33:29 -0500

    adding emit

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,27 +1,38 @@
 <!-- frontend/src/App.vue -->
 <template>
     <div id="app">
+      <AddressInput @data-updated="handleDataUpdate" />
       <StoreMap 
         @store-added="handleStoreAdded"
         @store-selected="handleStoreSelected"
       />
     </div>
+
+
   </template>
   
   <script>
+  import AddressInput from './components/AddressInput.vue';
+import AddressInput from './components/AddressInput.vue';
   import StoreMap from './components/StoreMap.vue'
+
+
   
   export default {
     name: 'App',
     components: {
-      StoreMap
+      StoreMap,
+      AddressInput
     },
     methods: {
       handleStoreAdded(position) {
-        console.log('New store added at:', position)
+        console.log('New store added at:', position);
       },
       handleStoreSelected(marker) {
-        console.log('Store selected:', marker)
+        console.log('Store selected:', marker);
+      },
+      handleDataUpdate(data) {
+       console.log(data); // 'Data from Child'
       }
     }
   }
---
+++ frontend/src/components/AddressInput.vue
@@ -0,0 +1,21 @@
+<template>
+    <!--
+    <p>Message is: {{ message }}</p>
+      <input v-model="message" placeholder="edit me" />
+    -->
+      <button @click="emitData">Send Data</button>
+  </template>
+
+<script>
+// import { ref } from 'vue'
+// const message = ref('')
+
+export default {
+       methods: {
+         emitData() {
+           this.$emit('data-updated', 'Data from Child');
+         }
+       }
+     }
+
+</script>
